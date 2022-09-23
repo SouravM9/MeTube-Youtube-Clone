3939282746a432855726a3b8e470ffb31447f45f
SouravM9 <majumdars598@gmail.com>
2022-09-23 19:22:20 +0530

    Youtube clone. header in progress.

---
+++ src/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Header.css';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import VideoCallOutlinedIcon from '@mui/icons-material/VideoCallOutlined';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import Avatar from "@mui/material/Avatar";
+
+function Header()
+{
+    return (
+        < div className="header">
+            <div className="header__left">
+            <MenuIcon />
+            <img className="header__logo" src="https://upload.wikimedia.org/wikipedia/commons/9/9e/YouTube_Logo_%282013-2017%29.svg" alt="YouTube Logo" />
+            </div>
+
+            <div className="header__input">
+            <input type="text" />
+            <SearchIcon />
+            </div>
+
+            <div className="header__right">
+            <VideoCallOutlinedIcon />
+            <NotificationsOutlinedIcon />
+            <Avatar alt="Sourav" src="https://media-exp1.licdn.com/dms/image/C5603AQGHve6wtWOvmw/profile-displayphoto-shrink_800_800/0/1642446054047?e=1669248000&v=beta&t=L0ELMHYNG6xLHx4v3qbjn1cPH9EuD0qHnis9PjDq8XI"/>
+            </div>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+/*
+ // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC-QlFu-h8DhfAegJEuo96omZWDe7uEGO0",
+  authDomain: "clone-9b636.firebaseapp.com",
+  projectId: "clone-9b636",
+  storageBucket: "clone-9b636.appspot.com",
+  messagingSenderId: "615691791130",
+  appId: "1:615691791130:web:37f4926c6f0227cb8adaa3",
+  measurementId: "G-32FHE0GKMZ"
+};
+  
+ * /
